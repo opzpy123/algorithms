52804070e7cd64e12f2ee057b4351cb7f2bbe745
赵鹏毅 <zhaopy@gantsoftware.com>
2024-02-04 15:42:54 +0800

    接雨水

--- leetcode/src/hard/TrappingRain.java
+++ leetcode/src/hard/TrappingRain.java
@@ -2,35 +2,27 @@ package hard;
 
 public class TrappingRain {
 
-
-    public static int trap(int[] height) {
-        if(height.length<=2) return 0;
-        //一层一层接雨水 从第一层开始 每两个大于等于1之间的方块可以接他们之间距离的水
-
-        int max=0;
-        for (int i = 0; i < height.length; i++) {
-            max= Math.max(height[i],max);
-        }
-
-        int sum = 0;
-        for (int i = 1; i <= max; i++) {
-            boolean isStart = false; //标记是否开始更新 temp
-            int temp_sum = 0;
-            for (int j = 0; j < height.length; j++) {
-                if (isStart && height[j] < i) {
-                    temp_sum++;
-                }
-                if (height[j] >= i) {
-                    sum = sum + temp_sum;
-                    temp_sum = 0;
-                    isStart = true;
-                }
-            }
-        }
-        return sum;
-    }
-
     public static void main(String[] args) {
         System.out.println(trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
     }
+
+    public static int trap(int[] height) {
+        int n = height.length;
+        int[] leftMax = new int[n];
+        leftMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            leftMax[i] = Math.max(leftMax[i - 1], height[i]);
+        }
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], height[i]);
+        }
+        int rst = 0;
+        for (int i = 0; i < n; i++) {
+            rst += Math.min(rightMax[i], leftMax[i]) - height[i];
+        }
+        return rst;
+    }
+
 }
